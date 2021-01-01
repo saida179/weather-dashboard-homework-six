d9e9b38b185491808e39932123540f62bac2158c
saida179 <69127945+saida179@users.noreply.github.com>
2021-01-01 12:38:15 -0700

    updating weather dashboard

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # weather-dashboard-homework-six
-I thought this homework was going to be easy but surprise it wasn't.
-there was a lot of class activity that I tried to get ideas on how to use 
-the JS for the API key, but of course I hit so rocks alone the way.
-I know there must be and easy way of writing the JS, but my brain 
-just goes blank when I'm doing this on my own and I know I will learn this third 
-language I just wish there was more time.
--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+//GIVEN a weather dashboard with form inputs
+//WHEN I search for a city
+//THEN I am presented with current and future conditions for that city and that city is added to the search history
+//WHEN I view current weather conditions for that city
+//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+//WHEN I view the UV index
+//THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+//WHEN I view future weather conditions for that city
+//THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+//WHEN I click on a city in the search history
+//THEN I am again presented with current and future conditions for that city
+//WHEN I open the weather dashboard
+//THEN I am presented with the last searched city forecast
+
 $(document).ready(function(){
-    const apiKey ="b04868ffacca8e05f416295d5d74ee96";
+    const apiKey ="a6ed307e0d393685de5ab5ab77958fdb";
     const loadLocal = window.localStorage.getItem("city")
     
     let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + APIKey;
